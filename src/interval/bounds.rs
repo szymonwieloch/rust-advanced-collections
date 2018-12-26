514bbe9350dfb2aa8936cc5652c645f299c23aaf
szymonwieloch <szymon.wieloch@gmail.com>
2018-12-26 01:10:35 +0100

    Full math support for bounds.

--- src/interval/bounds.rs
+++ src/interval/bounds.rs
@@ -1,20 +1,18 @@
 use std::cmp::Ordering;
 use self::Ordering::*;
 
-//LowerBound ======================================================
+use std::ops::{Add, AddAssign, Sub, SubAssign,  Mul, MulAssign, Div, DivAssign, Deref, DerefMut};
+
+
+// Bound =============================================
 
 #[derive(Clone, Debug, Copy, Eq, PartialEq, Hash)]
-pub struct LowerBound<T> where T: Ord {
+pub struct Bound<T> where T: Ord {
     val: T,
     is_closed: bool
 }
 
-impl <T> LowerBound<T> where T: Ord {
-
-    pub fn is_separated_from(&self, other: &UpperBound<T>) -> bool {
-        are_separated(self, other)
-    }
-
+impl <T> Bound<T> where T: Ord {
     pub fn val(&self) -> &T {
         &self.val
     }
@@ -34,6 +32,122 @@ impl <T> LowerBound<T> where T: Ord {
         (self.val, self.is_closed)
     }
 }
+
+impl<T, U> Add<U> for Bound<T> where T: Ord + Add<U, Output=T> {
+    type Output = Bound<T>;
+
+    fn add(self, rhs: U) -> <Self as Add<U>>::Output {
+        Bound {
+            val: self.val + rhs,
+            is_closed: self.is_closed
+        }
+    }
+}
+
+impl<T, U> Sub<U> for Bound<T> where T: Ord + Sub<U, Output=T> {
+    type Output = Bound<T>;
+
+    fn sub(self, rhs: U) -> <Self as Sub<U>>::Output {
+        Bound {
+            val: self.val - rhs,
+            is_closed: self.is_closed
+        }
+    }
+}
+
+impl<T, U> Mul<U> for Bound<T> where T: Ord + Mul<U, Output=T> {
+    type Output = Bound<T>;
+
+    fn mul(self, rhs: U) -> <Self as Mul<U>>::Output {
+        Bound {
+            val: self.val * rhs,
+            is_closed: self.is_closed
+        }
+    }
+}
+
+impl<T, U> Div<U> for Bound<T> where T: Ord + Div<U, Output=T> {
+    type Output = Bound<T>;
+
+    fn div(self, rhs: U) -> <Self as Div<U>>::Output {
+        Bound {
+            val: self.val / rhs,
+            is_closed: self.is_closed
+        }
+    }
+}
+
+impl<T, U> AddAssign<U> for Bound<T> where T:Ord + AddAssign<U> {
+    fn add_assign(&mut self, rhs: U) {
+        self.val += rhs;
+    }
+}
+
+impl<T, U> SubAssign<U> for Bound<T> where T:Ord + SubAssign<U> {
+    fn sub_assign(&mut self, rhs: U) {
+        self.val -= rhs;
+    }
+}
+
+impl<T, U> MulAssign<U> for Bound<T> where T:Ord + MulAssign<U> {
+    fn mul_assign(&mut self, rhs: U) {
+        self.val *= rhs;
+    }
+}
+
+impl<T, U> DivAssign<U> for Bound<T> where T:Ord + DivAssign<U> {
+    fn div_assign(&mut self, rhs: U) {
+        self.val /= rhs;
+    }
+}
+
+//LowerBound ======================================================
+
+#[derive(Clone, Debug, Copy, Eq, PartialEq, Hash)]
+pub struct LowerBound<T> where T: Ord {
+    bound: Bound<T>
+}
+
+impl <T> LowerBound<T> where T: Ord {
+
+    pub fn is_separated_from(&self, other: &UpperBound<T>) -> bool {
+        are_separated(self, other)
+    }
+
+    pub fn val(&self) -> &T {
+        &self.val
+    }
+
+    pub fn is_closed(&self) -> bool {
+        self.is_closed
+    }
+
+    pub fn new(val: T, is_closed: bool) -> Self {
+        Self {
+            bound: Bound::new(val, is_closed)
+        }
+    }
+
+    pub fn into_tuple(self) -> (T, bool) {
+        self.bound.into_tuple()
+    }
+}
+
+impl <T> Deref for LowerBound <T> where T: Ord {
+    type Target = Bound<T>;
+
+    fn deref(&self) -> &<Self as Deref>::Target {
+        &self.bound
+    }
+}
+
+/*
+impl <T> DerefMut for LowerBound<T> where T: Ord {
+    fn deref_mut(&mut self) -> &mut <Self as Deref>::Target {
+        unimplemented!()
+    }
+}
+*/
 
 impl<T> Ord for LowerBound<T> where T: Ord {
     fn cmp(&self, other: &Self) -> Ordering {
@@ -121,13 +235,76 @@ impl <T> PartialEq<UpperBound<T>> for LowerBound<T> where T: Ord {
     }
 }
 
+impl<T, U> Add<U> for LowerBound<T> where T: Ord + Add<U, Output=T> {
+    type Output = Self;
+
+    fn add(self, rhs: U) -> <Self as Add<U>>::Output {
+        Self{
+            bound: self.bound + rhs
+        }
+    }
+}
+
+impl<T, U> AddAssign<U> for LowerBound<T> where T: Ord + AddAssign<U> {
+    fn add_assign(&mut self, rhs: U) {
+        self.bound += rhs;
+    }
+}
+
+impl<T, U> Sub<U> for LowerBound<T> where T: Ord + Sub<U, Output=T> {
+    type Output = Self;
+
+    fn sub(self, rhs: U) -> <Self as Sub<U>>::Output {
+        Self{
+            bound: self.bound - rhs
+        }
+    }
+}
+
+impl<T, U> SubAssign<U> for LowerBound<T> where T: Ord + SubAssign<U> {
+    fn sub_assign(&mut self, rhs: U) {
+        self.bound -= rhs;
+    }
+}
+
+impl<T, U> Mul<U> for LowerBound<T> where T: Ord + Mul<U, Output=T> {
+    type Output = Self;
+
+    fn mul(self, rhs: U) -> <Self as Mul<U>>::Output {
+        Self{
+            bound: self.bound * rhs
+        }
+    }
+}
+
+impl<T, U> MulAssign<U> for LowerBound<T> where T: Ord + MulAssign<U> {
+    fn mul_assign(&mut self, rhs: U) {
+        self.bound *= rhs;
+    }
+}
+
+impl<T, U> Div<U> for LowerBound<T> where T: Ord + Div<U, Output=T> {
+    type Output = Self;
+
+    fn div(self, rhs: U) -> <Self as Div<U>>::Output {
+        Self{
+            bound: self.bound / rhs
+        }
+    }
+}
+
+impl<T, U> DivAssign<U> for LowerBound<T> where T: Ord + DivAssign<U> {
+    fn div_assign(&mut self, rhs: U) {
+        self.bound /= rhs;
+    }
+}
+
 
 //UpperBound ======================================================
 
 #[derive(Clone, Debug, Copy, Eq, PartialEq, Hash)]
 pub struct UpperBound<T> where T: Ord {
-    val: T,
-    is_closed: bool
+    bound: Bound<T>
 }
 
 impl <T> UpperBound<T> where T: Ord {
@@ -135,22 +312,22 @@ impl <T> UpperBound<T> where T: Ord {
         are_separated(other, self)
     }
 
-    pub fn val(&self) -> &T {
-        &self.val
-    }
-
-    pub fn is_closed(&self) -> bool {
-        self.is_closed
-    }
-
     pub fn new(val: T, is_closed: bool) -> Self{
         Self {
-            val, is_closed
+            bound: Bound::new(val, is_closed)
         }
     }
 
     pub fn into_tuple(self) -> (T, bool) {
-        (self.val, self.is_closed)
+        self.bound.into_tuple()
+    }
+}
+
+impl <T> Deref for UpperBound<T> where T: Ord {
+    type Target = Bound<T>;
+
+    fn deref(&self) -> &<Self as Deref>::Target {
+        &self.bound
     }
 }
 
@@ -240,13 +417,78 @@ impl <T> PartialEq<LowerBound<T>> for UpperBound<T> where T: Ord {
     }
 }
 
-// Helpers
+impl<T, U> Add<U> for UpperBound<T> where T: Ord + Add<U, Output=T> {
+    type Output = Self;
+
+    fn add(self, rhs: U) -> <Self as Add<U>>::Output {
+        Self{
+            bound: self.bound + rhs
+        }
+    }
+}
+
+impl<T, U> AddAssign<U> for UpperBound<T> where T: Ord + AddAssign<U> {
+    fn add_assign(&mut self, rhs: U) {
+        self.bound += rhs;
+    }
+}
+
+impl<T, U> Sub<U> for UpperBound<T> where T: Ord + Sub<U, Output=T> {
+    type Output = Self;
+
+    fn sub(self, rhs: U) -> <Self as Sub<U>>::Output {
+        Self{
+            bound: self.bound - rhs
+        }
+    }
+}
+
+impl<T, U> SubAssign<U> for UpperBound<T> where T: Ord + SubAssign<U> {
+    fn sub_assign(&mut self, rhs: U) {
+        self.bound -= rhs;
+    }
+}
+
+impl<T, U> Mul<U> for UpperBound<T> where T: Ord + Mul<U, Output=T> {
+    type Output = Self;
+
+    fn mul(self, rhs: U) -> <Self as Mul<U>>::Output {
+        Self{
+            bound: self.bound * rhs
+        }
+    }
+}
+
+impl<T, U> MulAssign<U> for UpperBound<T> where T: Ord + MulAssign<U> {
+    fn mul_assign(&mut self, rhs: U) {
+        self.bound *= rhs;
+    }
+}
+
+impl<T, U> Div<U> for UpperBound<T> where T: Ord + Div<U, Output=T> {
+    type Output = Self;
+
+    fn div(self, rhs: U) -> <Self as Div<U>>::Output {
+        Self{
+            bound: self.bound / rhs
+        }
+    }
+}
+
+impl<T, U> DivAssign<U> for UpperBound<T> where T: Ord + DivAssign<U> {
+    fn div_assign(&mut self, rhs: U) {
+        self.bound /= rhs;
+    }
+}
+
+// Helpers ==================================================================
 
 fn  are_separated<T>(l: &LowerBound<T>, u: &UpperBound<T>) -> bool where T: Ord{
     l.val() > u.val() || (u.val() == l.val() && !u.is_closed() && ! l.is_closed())
 }
 
 
+// Tests ======================================================================
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
@@ -314,5 +556,57 @@ mod tests {
         assert_ne!(UpperBound::new(5, false), LowerBound::new(5, false));
         assert!(UpperBound::new(5, true)>LowerBound::new(5, false));
         assert!(UpperBound::new(5, true)>LowerBound::new(4, true));
+    }
+
+    #[test]
+    fn test_add(){
+        let mut l = LowerBound::new(6, false);
+        assert_eq!(l+4, LowerBound::new(10, false));
+        l+= 3;
+        assert_eq!(l, LowerBound::new(9, false));
+
+        let mut u = UpperBound::new(6, false);
+        assert_eq!(u+4, UpperBound::new(10, false));
+        u+= 3;
+        assert_eq!(u, UpperBound::new(9, false));
+    }
+
+    #[test]
+    fn test_sub(){
+        let mut l = LowerBound::new(6, false);
+        assert_eq!(l-2, LowerBound::new(4, false));
+        l-= 3;
+        assert_eq!(l, LowerBound::new(3, false));
+
+        let mut u = UpperBound::new(6, false);
+        assert_eq!(u-2, UpperBound::new(4, false));
+        u-= 3;
+        assert_eq!(u, UpperBound::new(3, false));
+    }
+
+    #[test]
+    fn test_mul(){
+        let mut l = LowerBound::new(6, false);
+        assert_eq!(l*2, LowerBound::new(12, false));
+        l*= -3;
+        assert_eq!(l, LowerBound::new(-18, false));
+
+        let mut u = UpperBound::new(6, false);
+        assert_eq!(u*2, UpperBound::new(12, false));
+        u*= -3;
+        assert_eq!(u, UpperBound::new(-18, false));
+    }
+
+    #[test]
+    fn test_div(){
+        let mut l = LowerBound::new(6, false);
+        assert_eq!(l/2, LowerBound::new(3, false));
+        l/= -3;
+        assert_eq!(l, LowerBound::new(-2, false));
+
+        let mut u = UpperBound::new(6, false);
+        assert_eq!(u/2, UpperBound::new(3, false));
+        u/= -3;
+        assert_eq!(u, UpperBound::new(-2, false));
     }
 }
